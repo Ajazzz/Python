46a7f567373fb75717ea841ab2f90139ae7215bb
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-05 16:48:09 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -4,6 +4,7 @@
 # NumPy is a fundamental library for many other scientific computing libraries in Python, including SciPy, Pandas, Matplotlib, and scikit-learn.
 
 # NumPy is designed to be efficient for numerical computations, and it provides a vast collection of functions for linear algebra, 
-# Fourier analysis, random number generation, and more. NumPy also provides functionality for working with various data types and formats, such as CSV, HDF5, and MATLAB files.
+# Fourier analysis, random number generation, and more. NumPy also provides functionality for working with various data types and formats, such as CSV, HDF5, 
+# and MATLAB files.
 
 One of the key features of NumPy is its ability to perform operations on large arrays and datasets quickly and efficiently. NumPy can take advantage of vectorization, which means that it can perform operations on entire arrays at once, rather than iterating through each element one at a time. This can make NumPy code much faster and more efficient than equivalent Python code.

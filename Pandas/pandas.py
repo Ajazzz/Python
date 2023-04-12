94f5fff75f9f0daae9e2ae9b96c42cec93836e92
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-12 14:21:40 +0530

    Update pandas.py

--- Pandas/pandas.py
+++ Pandas/pandas.py
@@ -1,1 +1,4 @@
+# When working  with the data stored in spreadsheets or databases in a tabular format , pandas is the right. 
+# With pandas librabry we can, clean, and process the data. In pandas, a data table is called as a DataFrame.
+
 

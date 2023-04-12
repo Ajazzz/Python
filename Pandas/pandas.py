7fdbd171f6b713e4647e455332a13fd6309c5b88
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-12 14:40:09 +0530

    Update pandas.py

--- Pandas/pandas.py
+++ Pandas/pandas.py
@@ -1,4 +1,10 @@
-# When working  with the data stored in spreadsheets or databases in a tabular format , pandas is the right. 
+# When working  with the data stored in spreadsheets or databases in a tabular format , pandas is the right tool. 
 # With pandas librabry we can, clean, and process the data. In pandas, a data table is called as a DataFrame.
 
 
+# Creating a DataFrame using DataFrame fucntion in pnadas.
+# We can create a data using a lists of dictionary where dictioanry keys can be used as column HEADERS and valus from a list can be used as columns of a DataFrame.
+
+data = pd.DataFrame{ 'Name':['Ajaz', 'Navaz','Mathew' ,'jeff],
+                 'age':[22,23,24,26]}
+                 

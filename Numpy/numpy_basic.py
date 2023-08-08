cf0ca8b8255a9697ac95d7a4b2e22e9092322407
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-08-08 09:53:28 +0530

    Update numpy_basic.py

--- Numpy/numpy_basic.py
+++ Numpy/numpy_basic.py
@@ -1,8 +1,8 @@
 
 
-'''NumPy is a popular Python library that is used for scientific computing and data analysis.'''
-''' It provides support for multidimensional arrays, matrices, and various mathematical functions to manipulate these arrays efficiently'''
-''' NumPy is a fundamental library for many other scientific computing libraries in Python, including SciPy, Pandas, Matplotlib, and scikit-learn.'''
+'''NumPy is a popular Python library that is used for scientific computing and data analysis.
+ It provides support for multidimensional arrays, matrices, and various mathematical functions to manipulate these arrays efficiently
+ NumPy is a fundamental library for many other scientific computing libraries in Python, including SciPy, Pandas, Matplotlib, and scikit-learn.'''
 
 # NumPy is designed to be efficient for numerical computations, and it provides a vast collection of functions for linear algebra, 
 # Fourier analysis, random number generation, and more. NumPy also provides functionality for working with various data types and formats, such as CSV, HDF5, 

e93554dfe1746dae934a864d5b0712322105a75e
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-08-09 14:20:05 +0530

    Update basics.py

--- Basics/basics.py
+++ Basics/basics.py
@@ -1,4 +1,7 @@
 ''' This file contents of the basic python learning.
 Learning python & becoming good at it is a great goal.
 
-Here are some simple steps to follow: '''
+Here are some simple steps to follow: 
+
+1) Understading the basics.
+'''
